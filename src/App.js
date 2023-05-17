e1279fd83f44a0ba8afbf2d0b6e7c577e77c4459
mansidhamelia <mansidhamelia6@gmail.com>
2023-05-17 16:56:29 -0700

    defining routes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <Product /> }
+])
+
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
