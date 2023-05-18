9f2c1b292d658dc655e9132a88540cad687cf07d
mansidhamelia <mansidhamelia6@gmail.com>
2023-05-18 10:50:45 -0700

    layout and nested roots

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  // Route,
+  RouterProvider,
+  // createRoutesFromElements
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import RootLayout from "./pages/Root";
 
+// const routeDefinition = createRoutesFromElements(
+//   // with jsx code
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Product />} />
+//   </Route>
+// )
+// const router = createBrowserRouter(routeDefinition)
 
+// object based approach
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/products', element: <Product /> }
-])
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products', element: <Product /> }
+    ]
+  },
 
+])
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom"
+import MainNavigation from "../components/MainNavigation"
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <Link to="/products">The list of products</Link>
         </div>
     )
 }
